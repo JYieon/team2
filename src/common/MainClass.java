aeb10b9bef8d27a94ef8e52c61c3ff1bb9bf0f0c
ChoiJiyeon <dkcl.8642@gmail.com>
2024-10-10 15:52:24 +0900

    main수정

--- src/common/MainClass.java
+++ src/common/MainClass.java
@@ -1,15 +1,20 @@
 package common;
 
+import 최지연.Rinse;
+
 import java.util.Scanner;
 
 public class MainClass {
 	public static void main(String[] args) {
 		//세탁기
 		Scanner input = new Scanner(System.in);
-		
 		int num;
 		
+		Rinse r = new Rinse();
+		
 		while(true) {
+			System.out.println("-----------------------------");
+			System.out.println("[세탁기]");
 			System.out.println("1. 전원");
 			System.out.println("2. 헹굼");
 			System.out.println("3. 탈수");
@@ -22,6 +27,9 @@ public class MainClass {
 			switch(num) {
 			case 1:
 			case 2:
+				
+				r.display();
+				break;
 			case 3:
 			case 4:
 			case 5:
